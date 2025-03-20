3def5a3e5c52a9661d95eea9054c3879e6a851f5
Miguel Cortes <andresabyac@gmail.com>
2025-03-20 15:09:37 -0500

    added primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 import { AppComponent } from './app.component';
 import { appReducer } from './store/reducers/app.reducer';
 import { AppRoutingModule } from './app-routing.module';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     StoreModule.forRoot(appReducer),
     StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
